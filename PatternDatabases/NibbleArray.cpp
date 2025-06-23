f42935c9384a5195dfe5e74663c33512e33e5c06
KrishS23 <165558849+KrishS23@users.noreply.github.com>
2025-06-23 09:51:58 +0530

    NibbleArray.cpp

--- PatternDatabases/NibbleArray.cpp
+++ PatternDatabases/NibbleArray.cpp
@@ -1,7 +1,3 @@
-//
-// Created by Lakshya Mittal on 17-02-2022.
-//
-
 #include "NibbleArray.h"
 
 using namespace std;
@@ -15,12 +11,9 @@ uint8_t NibbleArray::get(const size_t pos) const {
     assert(pos <= this->size);
     uint8_t val = this->arr.at(i);
 
-//    Odd pos: last 4 bits
     if (pos % 2) {
         return val & 0x0F;
-    }
-//    Even pos: first 4 bits from the left
-    else {
+    } else {
         return val >> 4;
     }
 }
@@ -32,13 +25,10 @@ void NibbleArray::set(const size_t pos, const uint8_t val) {
 
     if (pos % 2) {
         this->arr.at(i) = (currVal & 0xF0) | (val & 0x0F);
-
     } else {
         this->arr.at(i) = (currVal & 0x0F) | (val << 4);
     }
 }
-
-//Get pointer to underlying array
 
 uint8_t *NibbleArray::data() {
     return this->arr.data();
@@ -52,17 +42,12 @@ size_t NibbleArray::storageSize() const {
     return this->arr.size();
 }
 
-// Move all the moves to a vector. This doubles the size, but is faster to access,
-// since there is no bitwise operation needed.
-
 void NibbleArray::inflate(vector<uint8_t> &dest) const {
     dest.reserve(this->size);
-
     for (unsigned i = 0; i < this->size; ++i)
         dest.push_back(this->get(i));
 }
 
-// Reset the array
 void NibbleArray::reset(const uint8_t val) {
     fill(this->arr.begin(), this->arr.end(), val);
 }
